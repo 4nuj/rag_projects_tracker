5c2c86abf4945d48385a35ebc56977ea770f987e
anuj tyagi <anuj.tyagii@gmail.com>
2024-06-10 18:34:12 +0100

    update

--- QandA.py
+++ QandA.py
@@ -1,149 +1,19 @@
 import boto3
 import streamlit as st
 
-# Initialize S3 client
-s3_client = boto3.client('s3')
-
-# Initialize Bedrock client
-bedrock_client = boto3.client('bedrock-agent-runtime', 'eu-west-3')
-
-st.set_page_config(
-    page_title="Project Tracker",
-    page_icon="📊",
-    layout="wide",
-)
-
-# CSS for custom styling
-st.markdown(
-    """
-    <style>
-    .sidebar .sidebar-content {
-        background-color: #2E2E2E;
-        color: white;
-    }
-    .stButton>button {
-        width: 100%;
-        border-radius: 15px;
-        background-color: #007BFF;
-        color: white;
-        font-size: 14px;
-        padding: 8px;
-    }
-    .stButton>button:hover {
-        background-color: #0056b3;
-        color: white;
-    }
-    .stTextInput>div>div>input {
-        font-size: 18px;
-        border-radius: 15px;
-        padding: 10px;
-    }
-    .stChatMessage {
-        border-radius: 15px;
-        padding: 10px;
-        margin: 5px 0;
-    }
-    .assistant {
-        background-color: #F8F9FA;
-        color: black;
-    }
-    .user {
-        background-color: #E8E8E8;  /* Changed background color */
-        color: black;
-    }
-    .highlight {
-        color: #FFDA33;
-    }
-    .error {
-        color: red;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-st.title("📊 Project Tracker")
-
-st.sidebar.title("Project Documents")
-selected_doc = None
-bucket_name = 'projectstracker'
-documents = []
-
-def list_s3_documents(bucket_name):
-    response = s3_client.list_objects_v2(Bucket=bucket_name)
-    documents = []
-    if 'Contents' in response:
-        for obj in response['Contents']:
-            key = obj['Key']
-            if key.endswith('.pdf'):
-                parts = key.split('_')
-                if len(parts) >= 2:
-                    company_name = parts[0]
-                    city = parts[1].replace('.pdf', '')
-                    documents.append({'company': company_name, 'city': city, 'key': key})
-    return documents
-
-documents = list_s3_documents(bucket_name)
+st.subheader('RAG Using Knowledge Base from Amazon Bedrock', divider='rainbow')
 
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 
-if 'selected_document' not in st.session_state:
-    st.session_state.selected_document = None
+for message in st.session_state.chat_history:
+    with st.chat_message(message['role']):
+        st.markdown(message['text'])
 
-if 'selected_workflow' not in st.session_state:
-    st.session_state.selected_workflow = []
+bedrockClient = boto3.client('bedrock-agent-runtime', 'eu-west-3')
 
-if 'query' not in st.session_state:
-    st.session_state.query = None
-
-if 'query_submitted' not in st.session_state:
-    st.session_state.query_submitted = False
-
-# Add a dropdown for projects
-project_options = [f"{doc['company']} - {doc['city']}" for doc in documents]
-selected_project = st.sidebar.selectbox("Select a Project", project_options)
-
-# Map the selected project to the document key
-for doc in documents:
-    if f"{doc['company']} - {doc['city']}" == selected_project:
-        st.session_state.selected_document = doc['key']
-        st.session_state.selected_company = doc['company']
-        st.session_state.selected_city = doc['city']
-        selected_doc = doc
-
-if st.session_state.selected_document:
-    st.sidebar.title("Workflow Options")
-    workflow_options = ["Tech used", "Key contacts", "Delivery dates"]
-    selected_workflow_options = st.sidebar.multiselect("Select Workflow Options", workflow_options)
-    st.session_state.selected_workflow = selected_workflow_options
-
-    st.sidebar.markdown("<br><br><br>", unsafe_allow_html=True)  # Add some space before the button
-    if st.sidebar.button("Submit"):
-        if st.session_state.selected_workflow:
-            query = f"Show me the {', '.join(st.session_state.selected_workflow).lower()} for {st.session_state.selected_city} projects."
-            st.session_state.query = query
-            st.session_state.query_submitted = True
-
-            # Reset the selected document and workflow
-            st.session_state.selected_document = None
-            st.session_state.selected_workflow = []
-
-# Add a text input for queries
-questions = st.text_input('Enter your questions here...', value=st.session_state.query if st.session_state.query else "")
-if questions:
-    st.session_state.query = questions
-    st.session_state.query_submitted = True
-
-if 'query' in st.session_state and st.session_state.query_submitted:
-    questions = st.session_state.query
-    st.session_state.query_submitted = False  # Reset before processing to avoid duplication
-
-    with st.chat_message('user'):
-        st.markdown(questions)
-    st.session_state.chat_history.append({"role": 'user', "text": questions})
-
-    response = bedrock_client.retrieve_and_generate(
+def getAnswers(questions):
+    knowledgeBaseResponse  = bedrockClient.retrieve_and_generate(
         input={'text': questions},
         retrieveAndGenerateConfiguration={
             'knowledgeBaseConfiguration': {
@@ -152,7 +22,43 @@ if 'query' in st.session_state and st.session_state.query_submitted:
             },
             'type': 'KNOWLEDGE_BASE'
         })
+    return knowledgeBaseResponse
 
+def list_s3_documents(bucket_name):
+    s3_client = boto3.client('s3')
+    response = s3_client.list_objects_v2(Bucket=bucket_name)
+    documents = []
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            key = obj['Key']
+            if key.endswith('.pdf'):  # Assuming documents are PDF files
+                parts = key.split('_')
+                if len(parts) >= 2:
+                    company_name = parts[0]
+                    city = parts[1]
+                    documents.append({'company': company_name, 'city': city, 'key': key})
+    return documents
+
+bucket_name = 'projectstracker'
+documents = list_s3_documents(bucket_name)
+
+st.sidebar.title("Project Documents")
+for doc in documents:
+    if st.sidebar.button(f"{doc['company']} - {doc['city']}"):
+        st.session_state.selected_document = doc['key']
+        st.session_state.selected_company = doc['company']
+        st.session_state.selected_city = doc['city']
+
+if 'selected_document' in st.session_state:
+    st.write(f"Selected Document: {st.session_state.selected_company} - {st.session_state.selected_city}")
+
+questions = st.chat_input('Enter your questions here...')
+if questions:
+    with st.chat_message('user'):
+        st.markdown(questions)
+    st.session_state.chat_history.append({"role": 'user', "text": questions})
+
+    response = getAnswers(questions)
     answer = response['output']['text']
 
     with st.chat_message('assistant'):
@@ -160,16 +66,12 @@ if 'query' in st.session_state and st.session_state.query_submitted:
     st.session_state.chat_history.append({"role": 'assistant', "text": answer})
 
     if len(response['citations'][0]['retrievedReferences']) != 0:
-        # context = response['citations'][0]['retrievedReferences'][0]['content']['text']
+        context = response['citations'][0]['retrievedReferences'][0]['content']['text']
         doc_url = response['citations'][0]['retrievedReferences'][0]['location']['s3Location']['uri']
 
-        # st.markdown(f"<span class='highlight'>Context used: </span>{context}", unsafe_allow_html=True)
-        st.markdown(f"<span class='highlight'>Source Document: </span>{doc_url}", unsafe_allow_html=True)
+        # Show the context and the document source for the latest Question Answer
+        st.markdown(f"<span style='color:#FFDA33'>Context used: </span>{context}", unsafe_allow_html=True)
+        st.markdown(f"<span style='color:#FFDA33'>Source Document: </span>{doc_url}", unsafe_allow_html=True)
 
     else:
-        st.markdown(f"<span class='error'>No Context</span>", unsafe_allow_html=True)
-
-for message in st.session_state.chat_history:
-    role_class = 'assistant' if message['role'] == 'assistant' else 'user'
-    with st.chat_message(message['role']):
-        st.markdown(f"<div class='stChatMessage {role_class}'>{message['text']}</div>", unsafe_allow_html=True)
+        st.markdown(f"<span style='color:red'>No Context</span>", unsafe_allow_html=True)
